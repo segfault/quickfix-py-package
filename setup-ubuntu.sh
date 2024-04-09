6d2d3f00dd88bdc42b00beee93af48bc423822e8
Pablo Carballo <pablo.carballo@revolut.com>
2024-04-09 18:09:52 +0200

    use script

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-apt install build-essential software-properties-common -y
-add-apt-repository ppa:deadsnakes/ppa
-apt update
-apt install git
-apt install autoconf
-apt install libtool
-apt install g++
-apt install python3.11 -y
-apt install python3.11-dev -y
-apt install libssl-dev -y
-apt install swig -y
+apt-get update -y
+apt-get install libssl-dev -y
+apt-get install swig -y
