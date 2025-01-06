8dd8406064b6f4ca70aa15a6537e0bb3066381fd
Mark Guzman <segfault@hasno.info>
2025-01-06 10:06:22 -0500

    reworking the build process. only current python and adding multiple architectures

--- setup-linux-yum.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-yum update -y
-yum install openssl-devel swig -y
-
-echo "Config libs: ${pkg-config --libs openssl}"
---
+++ setup-os.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+BREW_BIN=$(which brew)
+if [ -x "$BREW_BIN" ]; then
+  brew update
+  brew install autoconf libtool swig cmake ninja openssl@1.1
+fi
+
+YUM_BIN=$(which yum)
+if [ -x "$YUM_BIN" ]; then
+  yum update -y
+  yum install openssl-devel cmake ninja swig -y
+
+  echo "Config libs: ${pkg-config --libs openssl}"
+fi
+
+APT_GET_BIN=$(which apt-get)
+if [ -x "$APT_GET_BIN" ]; then
+  apt-get update -y
+  apt-get install libssl-dev swig cmake ninja -y
+
+  echo "Config libs: ${pkg-config --libs openssl}"
+fi
